bc9deb35fdbef4cbca7e84f6a0497aa0fe72fa0c
LarsteBooy <36959162+LarsteBooy@users.noreply.github.com>
2018-11-07 20:41:32 +0100

    added some imports to the home.module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { AppModule } from '../../app.module'
+import { HomeRoutingModule } from './home.routing';
+import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRoutingModule,
+    AppModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, AdminLoginComponent]
 })
 export class HomeModule { }
