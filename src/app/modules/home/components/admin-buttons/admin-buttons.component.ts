60ca18fb46bdc06fe5cbb245385da52c7f39b5da
Lars <lars.tebooy@hotmail.com>
2018-11-21 10:43:50 +0100

    Hide buttons when not an admin
    * Hide buttons when not an admin
    * Fixed the margin of the list

--- src/app/modules/home/components/admin-buttons/admin-buttons.component.ts
+++ src/app/modules/home/components/admin-buttons/admin-buttons.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { LOCAL_STORAGE_USER_ROLE } from '../../../../shared/constants';
 
 @Component({
-  selector: 'admin-buttons',
+  selector: 'app-admin-buttons',
   templateUrl: './admin-buttons.component.html',
   styleUrls: ['./admin-buttons.component.css']
 })
 export class AdminButtonsComponent implements OnInit {
 
+  isAdmin: boolean;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setAdmin(): void {
+    localStorage.setItem(LOCAL_STORAGE_USER_ROLE, 'Admin');
+    this.isAdmin = true;
+  }
+
+  setUser(): void {
+    localStorage.setItem(LOCAL_STORAGE_USER_ROLE, 'User');
+    this.isAdmin = false;
   }
 
 }
